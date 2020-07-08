0fa8967445fcd966a01c0f0f8c9d5926c2b424e4
Anubhav Solanki <anubhavsolanki0@gmail.com>
2020-07-09 03:11:50 +0530

    first deploy

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+var count=0;
+
+// Start the Server
+http.listen(port, function () {
+    console.log('Server Started. Listening on http://localhost:' + port);
+});
+
+
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+      }
+      next();
+    }
+  }
+  
+
+app.use(forceSSL());
+
+app.use(express.static(__dirname + '/dist'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+//SOCKET
+io.on('connection', function (socket) {
+    console.log('No. of sockets ' + ++count);
+    socket.on('disconnect', function() {
+      --count;
+      console.log('One person disconnected!');
+      console.log('Now, no. of sockets : ' + count);
+   });
+    
+   //GETTING DATA FROM THE CLIENTSIDE FRONTEND 
+   socket.on("sendMyDetails",(myDetails)=>{
+        io.emit(myDetails.name + '-' + myDetails.friendName ,{
+            longitude : myDetails.longitude,
+            latitude : myDetails.latitude
+        });
+   })
+});
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,138 @@
+import { Component, OnInit,ViewChild, ElementRef } from '@angular/core';
+import {AgmMap  } from '@agm/core';
+import * as io from 'socket.io-client';
+import {} from 'googlemaps';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent implements OnInit {
+  @ViewChild(AgmMap,{static: true}) public agmMap: AgmMap;
+  @ViewChild('myName') myName: ElementRef;
+  @ViewChild('friendName') friendName: ElementRef;
+  socket: SocketIOClient.Socket;
+  myLocationLongitude:number;
+  myLocationLatitude:number;
+  my_name:string;
+  friend_name:string;
+  friendLongitude:number;
+  friendLatitude:number;
+  zoom:number = 14;
+  myIcon:string='https://img.icons8.com/color/40/000000/marker.png';
+  friendIcon:string='https://img.icons8.com/color/48/000000/boy-avatar.png';
+
+  constructor() {
+    this.socket = io.connect('http://localhost:3000/');
+  }
+
+  
+
+
+  getMylocation() {  
+    if (navigator.geolocation) {  
+        navigator.geolocation.getCurrentPosition((position: Position) => {  
+            if (position) { 
+              this.myLocationLatitude = position.coords.latitude;
+              this.myLocationLongitude = position.coords.longitude;  
+            } 
+        })  
+    }
+  }
+  
+
+
+  onMouseOver(infoWindow) {
+    infoWindow.open();
+  }
+  onMouseOut(infoWindow) {
+    infoWindow.close();
+  }
+  
+  
+  moveMarker(i,previousLocation,delta){
+    {
+        previousLocation.longitude += delta.longitude;
+        previousLocation.latitude += delta.latitude;
+        this.friendLatitude = previousLocation.latitude;
+        this.friendLongitude = previousLocation.longitude;
+        if(i <= 100){
+          i++;
+          setTimeout(()=>{this.moveMarker(i,previousLocation,delta);},10);
+        }
+     }
+   }
+  
+  
+
+
+  scrollFunction(id){
+    let el = document.getElementById(id);
+    el.scrollIntoView();
+  }
+
+ 
+  animate(friendLocation,previousLocation){
+    let i=0;
+    let delta = {
+    latitude : (friendLocation.latitude - previousLocation.latitude)/100,
+    longitude : (friendLocation.longitude - previousLocation.longitude)/100
+    }
+    this.moveMarker(i,previousLocation,delta);
+  }
+ 
+
+  showfriendLocation(){
+    let runOnce =true;
+    this.my_name =this.myName.nativeElement.value;
+    this.friend_name = this.friendName.nativeElement.value;
+    let previousLocation={
+      latitude : 123,
+      longitude : 456
+    };
+    let myDetails = {
+      name : this.my_name,
+      friendName : this.friend_name,
+      latitude : this.myLocationLatitude,
+      longitude : this.myLocationLongitude
+    }
+    document.getElementById('mapBox').style.display = "block";
+    this.socket.emit("sendMyDetails",myDetails);
+    this.socket.on(this.friend_name+'-'+this.my_name,friendLocation=>{
+      if(runOnce){
+        this.friendLatitude = friendLocation.latitude;
+        this.friendLongitude = friendLocation.longitude;
+        runOnce = false;
+        previousLocation.latitude = friendLocation.latitude;
+        previousLocation.longitude = friendLocation.longitude;
+        this.socket.emit("sendMyDetails",myDetails);
+      }else{
+        this.animate(friendLocation,previousLocation);
+      }
+    });
+  }
+
+
+
+  ngOnInit(): void {
+    this.getMylocation();
+    if(navigator.geolocation){
+      navigator.geolocation.watchPosition(position=>{
+        let myDetails = {
+          name : this.my_name,
+          friendName : this.friend_name,
+          latitude : position.coords.latitude,
+          longitude : position.coords.longitude
+        };
+        this.socket.emit("sendMyDetails",myDetails);
+      } ,()=>{console.log("error occured");} , {
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: 0
+      });
+    }
+  }
+
+}
